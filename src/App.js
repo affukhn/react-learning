39c93e3222022bddb3e20cb24264666b4b65ff19
affukhn <arfkhnsuk@gmail.com>
2023-07-11 14:28:15 +0530

    final_update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useReducer } from "react";
-import Component1 from "./component/Component1";
-import Component2 from "./component/Component2";
-import Component3 from "./component/Component3";
+import React from "react";
+import FocusInput from "./Callback/FocusInput";
 
 // export const UserContext = React.createContext();
 // export const ChannelContext = React.createContext();
@@ -22,35 +20,37 @@ const reducer = (state, action) => {
 };
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
+  // const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <CountContext.Provider
-      value={{ countState: count, countDispatch: dispatch }}
-    >
-      <div>
-        count - {count}
-        {/* <CounterOne /> */}
-        {/* <CounterTwo /> */}
-        {/* <CounterThree /> */}
-        <Component1 />
+    // <CountContext.Provider
+    //   value={{ countState: count, countDispatch: dispatch }}
+    // >
+    <div>
+      {/* <ParentComponent /> */}
+      <FocusInput />
+      {/* count - {count} */}
+      {/* <CounterOne /> */}
+      {/* <CounterTwo /> */}
+      {/* <CounterThree /> */}
+      {/* <Component1 />
         <Component2 />
-        <Component3 />
-        {/* <UserContext.Provider value={"Arfath"}>
+        <Component3 /> */}
+      {/* <UserContext.Provider value={"Arfath"}>
         <ChannelContext.Provider value={"Suk"}>
           <ComponentA />
         </ChannelContext.Provider>
       </UserContext.Provider> */}
-        {/* <DataFetching></DataFetching> */}
-        {/* <IntervalHook /> */}
-        {/* <MouseContainer /> */}
-        {/* <HookMouse /> */}
-        {/* <HookCounterOne /> */}
-        {/* <HookCount /> */}
-        {/* <HookCounterTwo /> */}
-        {/* <HookCounterThree /> */}
-        {/* <HookCounterFour /> */}
-      </div>
-    </CountContext.Provider>
+      {/* <DataFetching></DataFetching> */}
+      {/* <IntervalHook /> */}
+      {/* <MouseContainer /> */}
+      {/* <HookMouse /> */}
+      {/* <HookCounterOne /> */}
+      {/* <HookCount /> */}
+      {/* <HookCounterTwo /> */}
+      {/* <HookCounterThree /> */}
+      {/* <HookCounterFour /> */}
+    </div>
+    // </CountContext.Provider>
   );
 }
 
